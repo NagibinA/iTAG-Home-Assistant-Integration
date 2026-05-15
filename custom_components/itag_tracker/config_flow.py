ccbd6f4224012eda323287c96eafc6f881b603bb
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-15 20:40:43 +0300

    Update config_flow.py

--- custom_components/itag_tracker/config_flow.py
+++ custom_components/itag_tracker/config_flow.py
@@ -7,17 +7,13 @@ from .const import DOMAIN
 
 
 def is_valid_mac(mac: str) -> bool:
-    """Проверка формата MAC-адреса."""
     return bool(re.match(r"^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$", mac))
 
 
 class iTAGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Настройка iTAG Tracker."""
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Шаг настройки: ввод имени и MAC."""
         errors = {}
 
         if user_input is not None:
@@ -31,22 +27,16 @@ class iTAGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             else:
                 await self.async_set_unique_id(mac)
                 self._abort_if_unique_id_configured()
-
                 return self.async_create_entry(
                     title=name,
-                    data={
-                        "name": name,
-                        "mac_address": mac,
-                    },
+                    data={"name": name, "mac_address": mac},
                 )
-
-        data_schema = vol.Schema({
-            vol.Required("name", default="iTAG"): str,
-            vol.Required("mac_address"): str,
-        })
 
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
+            data_schema=vol.Schema({
+                vol.Required("name", default="iTAG"): str,
+                vol.Required("mac_address"): str,
+            }),
             errors=errors,
         )
