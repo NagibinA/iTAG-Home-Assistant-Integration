b2687380bffa67ea376d34c551fc903650f014a2
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-15 23:21:35 +0300

    Update const.py

--- custom_components/itag_tracker/const.py
+++ custom_components/itag_tracker/const.py
@@ -1,28 +1,10 @@
-"""Константы для iTAG Tracker."""
-
+"""Constants for the iTAG integration."""
 DOMAIN = "itag_tracker"
-MANUFACTURER = "iTAG"
-MODEL = "BLE Tracker"
+DEFAULT_NAME = "iTAG"
+RSSI_PRESENCE_THRESHOLD = -80
+RSSI_CONNECT_THRESHOLD = -60
+CONNECT_TIMEOUT = 10.0
 
-# Время обновления (секунды)
-SCAN_INTERVAL = 10
-
-# Пороги RSSI
-RSSI_PRESENCE_THRESHOLD = -80      # Дома/Не дома
-RSSI_CONNECT_THRESHOLD = -60       # Порог для активного подключения
-
-# Таймауты
-CONNECTION_TIMEOUT = 30            # Секунд без сигнала -> отключиться
-DISCONNECT_DELAY = 60              # Секунд после потери сигнала
-
-# UUID для iTAG (по данным из BLE Monitor)
-BATTERY_SERVICE_UUID = "0000180f-0000-1000-8000-00805f9b34fb"
-BATTERY_CHAR_UUID = "00002a19-0000-1000-8000-00805f9b34fb"
-BUTTON_SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb"
-BUTTON_CHAR_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
-
-# Альтернативные UUID (если стандартные не работают)
-ALT_BUTTON_CHAR_UUID = "0000fff2-0000-1000-8000-00805f9b34fb"
-
-# Сервисы
-SERVICES = []
+# Service UUIDs
+BATTERY_SERVICE_UUID = "00002a19-0000-1000-8000-00805f9b34fb"
+BUTTON_SERVICE_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
