9eee898ae91fce42e4db3afb3c6bef8fcd426c9c
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-15 22:28:07 +0300

    Update config_flow.py

--- custom_components/itag_tracker/config_flow.py
+++ custom_components/itag_tracker/config_flow.py
@@ -3,17 +3,21 @@
 import re
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers import device_registry as dr
+
 from .const import DOMAIN
 
-
 def is_valid_mac(mac: str) -> bool:
+    """Проверка формата MAC-адреса."""
     return bool(re.match(r"^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$", mac))
 
-
 class iTAGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Настройка iTAG Tracker."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Шаг настройки: ввод имени и MAC."""
         errors = {}
 
         if user_input is not None:
@@ -29,14 +33,19 @@ class iTAGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=name,
-                    data={"name": name, "mac_address": mac},
+                    data={
+                        "name": name,
+                        "mac_address": mac,
+                    },
                 )
+
+        data_schema = vol.Schema({
+            vol.Required("name", default="iTAG"): str,
+            vol.Required("mac_address"): str,
+        })
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("name", default="iTAG"): str,
-                vol.Required("mac_address"): str,
-            }),
+            data_schema=data_schema,
             errors=errors,
         )
