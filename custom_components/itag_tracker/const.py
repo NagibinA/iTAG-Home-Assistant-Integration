a60b3618bc39cfa574bbb969efee501127f1982b
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-16 12:20:56 +0300

    Update const.py

--- custom_components/itag_tracker/const.py
+++ custom_components/itag_tracker/const.py
@@ -2,5 +2,6 @@
 DOMAIN = "itag_tracker"
 DEFAULT_NAME = "iTAG"
 CONNECT_TIMEOUT = 10.0
+RSSI_OFFLINE_VALUE = -120  # Значение RSSI при отсутствии сигнала
 
 BATTERY_SERVICE_UUID = "00002a19-0000-1000-8000-00805f9b34fb"
