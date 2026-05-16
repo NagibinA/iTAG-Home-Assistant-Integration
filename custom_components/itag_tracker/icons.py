45cb01767fd617aa97e37c281a968c1600421ca3
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-16 14:12:43 +0300

    Update icons.py

--- custom_components/itag_tracker/icons.py
+++ custom_components/itag_tracker/icons.py
@@ -4,8 +4,8 @@ def get_icon(icon_name: str, state=None) -> str:
     """Return icon for given name."""
     if icon_name == "battery_sensor":
         return "mdi:battery"
-    
+
     if icon_name == "rssi_sensor":
         return "mdi:signal"
-    
+
     return "mdi:bluetooth"
